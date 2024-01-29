cad0b8e2ef856dd10fcb00a5020cda4cea89385d
sumanth-21 <saisumanthgoud.p@gmail.com>
2024-01-29 17:06:14 -0500

    client deployment fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  build: {
-    outDir: "../server/public",
-    emptyOutDir: true,
-  },
   server: {
     proxy: {
       "/api": {
